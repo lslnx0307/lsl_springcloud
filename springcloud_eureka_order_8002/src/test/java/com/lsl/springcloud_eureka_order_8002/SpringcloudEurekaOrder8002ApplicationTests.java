eccbf29d414aadf4407463a810529253f317f199
v-jasperli <184576454@qq.com>
2019-04-25 20:05:29 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml
    #	springcloud_eureka_client_order_8002/src/main/resources/application.properties
    #	springcloud_eureka_client_order_8002/src/test/java/com/lsl/springclound_eureka_client_order_8002/SpringcloundEurekaClientOrder8002ApplicationTests.java
    #	springcloud_ribbon/src/main/java/com/lsl/springcloud_eureka_client_order_8002/SpringcloudEurekaClientOrder8002Application.java
    #	springcloud_ribbon/src/test/java/com/lsl/springcloud_eureka_client_order_8002/SpringcloudEurekaClientOrder8002ApplicationTests.java
    #	springcloud_ribbon/src/test/java/com/lsl/springcloud_ribbon/SpringcloudRibbonApplicationTests.java

---
+++ springcloud_eureka_order_8002/src/test/java/com/lsl/springcloud_eureka_order_8002/SpringcloudEurekaOrder8002ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lsl.springcloud_eureka_order_8002;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcloudEurekaOrder8002ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
